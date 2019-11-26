46d3df6c92ca656cc9d9299ff3a546bf2f0599f0
usamasaleem <usama1221999@gmail.com>
2019-11-26 22:33:26 +0500

    Added Admin File Structure

---
+++ Admin/admin_panel/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import logo from './assets/images/logo192.png'; 
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Usama</h1>
+      <img src={logo}></img>
+    </div>
+  );
+}
+
+export default App;
