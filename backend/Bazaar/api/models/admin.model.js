28102aa4e391d18146b0871cb41819ca310ec5d9
naumanshk4 <naumanshk4@gmail.com>
2019-12-01 17:44:04 +0500

    Added backend

---
+++ backend/Bazaar/api/models/admin.model.js
@@ -0,0 +1,19 @@
+var mongoose = require('mongoose');
+
+
+var adminSchema = new mongoose.Schema({
+    email : {
+      type : String,
+      required : true,
+      unique:true
+      
+    },
+    password: {
+        type: String,
+        required: true,
+      }
+    
+   
+  });
+
+  mongoose.model('admin', adminSchema,'admin');
---
+++ backend/Bazaar/api/routes/index.js
@@ -0,0 +1,54 @@
+var express=require('express');
+var router=express.Router();
+// var ctrlHotels = require('../controller/hotels.controllers.js');
+// var ctrlReviews = require('../controller/ctrlReviews.js');
+var adminCtrl=require('../controllers/admin.controller.js');
+var productCtrl=require('../controllers/product.controller.js');
+// var retCtrl=require("../controllers/retailer.controller.js");
+
+
+router
+.route('/admin/login')
+// .post(adminCtrl.signup)
+.post(adminCtrl.login);
+
+router
+.route('/test')
+.post(productCtrl.addProduct)
+.get(productCtrl.viewProduct)
+
+
+/// Retailers Request///
+router
+.route('/admin/retailers/request')
+.get(adminCtrl.ctrlGetAllRetailers)
+
+router
+.route('/admin/retailers/request/:retailerlID')
+.get(adminCtrl.ctrlGetOneRetailer)
+// .put(ctrlHotels.ctrlUpdatetOne)
+// .delete(ctrlHotels.hotelsDeleteOne);
+
+router
+.route('/admin/retailers/request/:retailerlID/stores')
+.get(adminCtrl.ctrlGetStores)
+
+//VERFIED RETAILERS ROUTES//
+router
+.route('/admin/retailers/viewAll')
+.get(adminCtrl.ctrlViewAllRetailer)
+// .put(ctrlHotels.ctrlUpdatetOne)
+// .delete(ctrlHotels.hotelsDeleteOne);
+
+router
+.route('/admin/retailers/view/:retailerlID')
+.get(adminCtrl.ctrlViewOneRetailer)
+// // .put(ctrlHotels.ctrlUpdatetOne)
+// // .delete(ctrlHotels.hotelsDeleteOne);
+
+router
+.route('/admin/retailers/view/:retailerlID/stores')
+.get(adminCtrl.verifiedRetailerStores)
+
+
+module.exports=router;
